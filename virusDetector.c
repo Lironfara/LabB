1e266f31e65363f9573476324f08c0238849640d
Lironfara <lironfarag34@gmail.com>
2024-12-01 14:09:43 +0200

    just need to fix memory leak

--- virusDetector.c
+++ virusDetector.c
@@ -5,6 +5,7 @@
 int MAX_LENGTH = 10000;
 int MAGIC_SIZE = 4;
 bool needToChange = false; //little need to change
+char* carray ;
 
 typedef struct virus{
     unsigned short SigSize; //the virus size to allocate
@@ -23,6 +24,7 @@ link* virus_list = NULL;
 virus* readVirus(FILE* file){
     virus* v = malloc(sizeof(virus));
     if (v == NULL){
+        free(v);
         return NULL;
     }
     fread(v, 1, 18, file); //for size and name
@@ -31,6 +33,7 @@ virus* readVirus(FILE* file){
     }
     v->sig = malloc(v->SigSize); 
     if (v->sig == NULL){
+        free(v->sig);
         free(v);
         return NULL;
     }
@@ -41,22 +44,7 @@ virus* readVirus(FILE* file){
 //05 00 56 49 52 55 53 00 00 00 00 00 00 00 00 00 00 00 31 32 33 34 35
 
 void printVirus(virus* virus, FILE* output){
-     printf("Enter output file name: ");
-    char filename[256];
-    //for getting the filename
-    if (fgets(filename, sizeof(filename), stdin) == NULL) {
-        printf("Failed to read filename.\n");
-        return;
-    }
-    filename[strcspn(filename, "\n")] = 0; //removing the newline
-    FILE *file = fopen(filename, "w"); //holds the data of all viruses
-    if (file == NULL){
-        printf("Error in opening file\n");
-        return;
-    }
-    printf("File opened\n");
     
-
     if (output == NULL){
         printf("%s",virus->virusName);
         printf("%x", virus->SigSize);
@@ -65,6 +53,7 @@ void printVirus(virus* virus, FILE* output){
         }
     }
     else{
+        printf("I am here\n");
         fprintf(output, "Virus name: %s\n", virus->virusName);
         fprintf(output, "Virus size: %d\n", virus->SigSize);
         fprintf(output, "signature:\n");
@@ -104,6 +93,7 @@ link* list_append(link* virus_list, virus* data){
 /* Free the memory allocated by the list. */
 void list_free(link *virus_list){
     if (virus_list == NULL){
+        free(virus_list);
         return;
     }
     link* current = virus_list;
@@ -114,7 +104,6 @@ void list_free(link *virus_list){
         free(current);
         current = nextVir;
     }
-    free(virus_list);
 }
 
 void load_signatures(){
@@ -172,18 +161,25 @@ void load_signatures(){
         }
     
     }
+    fclose(file);
+    printf("Loaded\n");
 }
 
+//all viruses in the file
 void detect_virus(char *buffer, unsigned int size, link *virus_list){
-   
+    bool found = false;
     link* v = virus_list;
-    while (v!=NULL){
-        link* current = v->nextVirus;
+    while (v != NULL){
         for (int i = 0; i < size; ++i){
-            if (memcmp(buffer + i, current->vir->sig, current->vir->SigSize) == 0)
-                printf("Virus detected: %s, at offset %d\n", current->vir->virusName, i);
+            if (memcmp(buffer + i, v->vir->sig, v->vir->SigSize) == 0){
+                found = true;
+                printf("Virus detected: %s, at offset %d\n", v->vir->virusName, i);
+            }         
         }
-        v = current;
+        v = v->nextVirus;
+    }
+    if (!found){
+        printf("No virus detected\n");
     }
 }
 
@@ -196,6 +192,7 @@ void detect_virus_helper (){
         printf("Failed to read filename.\n");
         return;
     }
+    filename[strcspn(filename, "\n")] = 0; //removing the newline
 
     FILE *infile = fopen(filename, "rb"); //holds the data of all viruses
     if (infile == NULL){
@@ -206,11 +203,89 @@ void detect_virus_helper (){
     char *buffer = malloc(10000);
     fread(buffer, 1, 10000, infile);
     detect_virus(buffer, 10000, virus_list);
+    free(buffer);
+}
+
+void neutralize_virus(char *fileName, int signatureOffset){
+    FILE *file = fopen(fileName, "r+b");
+    if (file == NULL){
+        printf("Error in opening file\n");
+        return;
+    }
+    int found = fseek(file, signatureOffset, SEEK_SET);
+    if (found != 0){
+        printf("Error in finding the signature\n");
+        fclose(file);
+        return;
+    }
+
+    unsigned char replace = 0xC3;
+    int switched = fwrite(&replace, sizeof(replace), 1, file);
+    if (switched != 1){
+        printf("Error in switching the signature\n");
+        fclose(file);
+        return;
+    }
+    else {
+        printf("Virus neutralized at offset %d.\n", signatureOffset);
+    }
+    fclose(file);
+}
+
+int* find_offset(char *buffer, unsigned int size, link *virus_list){
+    int* offsets = malloc(sizeof(int)*10000);
+    if(offsets == NULL){
+        free(offsets);
+        return NULL;
+    }
+    int counter = 0;
+    link* v = virus_list;
+    while (v != NULL){
+        for (int i = 0; i < size; ++i){
+            if (memcmp(buffer + i, v->vir->sig, v->vir->SigSize) == 0){
+                offsets[counter] = i;
+                counter++;
+            } 
+        }
+        v = v->nextVirus;
+    }
+    offsets[counter] = -1;
+    return offsets;
 }
 
 void fix_file(){
-    printf("Not implemented\n");
+    printf("Enter the file name to fix : ");
+    char fileName[10000];
+    //for getting the filename
+    if (fgets(fileName, sizeof(fileName), stdin) == NULL) {
+        printf("Failed to read filename.\n");
+        return;
+    }
+    fileName[strcspn(fileName, "\n")] = 0; //removing the newline
+
+    FILE *infile = fopen(fileName, "rb"); 
+    if (infile == NULL){
+        printf("Error in opening file\n");
+        return;
+    }
+
+    char *buffer = malloc(10000);
+    fread(buffer, 1, 10000, infile);
+    int* offsets = find_offset(buffer, 10000, virus_list); //loactions of the viruses
+    int counter = 0;
+    while(offsets[counter]!=-1){
+        int signatureOffset = offsets[counter];
+        neutralize_virus(fileName, signatureOffset);
+        counter++;
+    }
+    free(offsets);
+    free(buffer);
+    fclose(infile);
 }
+
+
+
+
 
 
 /*Maybe change the siganture so it will take the virus_list? */
@@ -218,21 +293,50 @@ void printSignature(){
     if (virus_list == NULL){
         return;
     }
+     printf("Enter output file name: ");
+    char filename[256];
+    //for getting the filename
+    if ( fgets(filename, sizeof(filename), stdin) == NULL) {
+        printf("Failed to read filename.\n");
+        return;
+    }
+    filename[strcspn(filename, "\n")] = 0; //removing the newline
 
+    FILE *file;
+    if (filename[0] == '\0') {
+        printf("No file name given.\n");
+        file = NULL;
+    }
+    else{
+        file = fopen(filename, "w"); //holds the data of all viruses
+    }
+
+    if (file == NULL){
+        printf("Error in opening file, printing to the screen\n");
+    }
+    printf("File opened\n");
+    
    link* current = virus_list;  // Start at the head of the list
     while (current != NULL) {
-        printVirus(current->vir, stdout);  // Print the virus data
+        printVirus(current->vir, file);  // Print the virus data
         current = current->nextVirus;     // Move to the next link
+    }
+    if (file!=NULL){
+        fclose(file);
     }
 }
 
 void stop_program(){
+        list_free(virus_list);
+        free(carray);
         printf("Exiting...\n");
         exit(0);
 }
 
 int main(int argc, char **argv) //stack arguments
 {
+    carray = malloc(5);
+    printf("\nWelcome to the virus detector\n");
 
     char inputBuffer[5];
     struct fun_desc {
@@ -240,7 +344,7 @@ int main(int argc, char **argv) //stack arguments
     void (*fun)();
     };
 
-    char* carray = malloc(5);
+    
     carray[0] = '\0'; //The pointer points to '0'
 
 
@@ -274,6 +378,5 @@ int main(int argc, char **argv) //stack arguments
 
     }
 
-    free(carray);
  
 }
